f1a2b84e0ef9d71a654114634ebb0f1aa044c491
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-02-22 20:10:49 +0200

    Lisätään muut tarvittavat komponentit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { BackbtnComponent } from './backbtn/backbtn.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    BackbtnComponent,
+    CartComponent,
+    ProductAlertsComponent,
+    ShippingComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
