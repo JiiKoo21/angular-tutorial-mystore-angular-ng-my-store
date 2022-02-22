3e3d307aeb98540f58d73c2016bdce95cb663df4
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-02-22 21:00:36 +0200

    Asennetaan Bootstrap, Popper ja Font Awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  
+  faCoffee=faCoffee;
 }
